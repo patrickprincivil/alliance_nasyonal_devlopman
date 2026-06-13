aa99d8b5c6a40e69d5e5c33982186df0963f4fcf
Patrick <patrick@email.com>
2026-06-13 18:48:42 -0300

    Clean Navbar links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,19 +1,35 @@
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <h2>🏛️ AND Platform</h2>
+    <header className="navbar">
+      <div>
+        <h2>Alliance Nasyonal</h2>
+      </div>
 
-      <div className="navlinks">
+      <nav className="navlinks">
         <a href="/">Home</a>
+        <a href="/about">About</a>
         <a href="/constitution">Constitution</a>
         <a href="/courts">Courts</a>
-        <a href="/justice-system">Justice System</a>
+        <a href="/journal">Journal</a>
+        <a href="/justice">Justice</a>
+        <a href="/public-money">Public Money</a>
+        <a href="/public-contracts">Contracts</a>
+        <a href="/hearings">Hearings</a>
+        <a href="/journalist-center">Journalists</a>
+        <a href="/farming">Farming</a>
+        <a href="/transport-industry">Transport</a>
+        <a href="/national-engineering">Engineering</a>
+        <a href="/programs">Programs</a>
+        <a href="/donations">Donations</a>
+        <a href="/radio">Radio</a>
+        <a href="/music">Music</a>
+        <a href="/digital-products">Digital Products</a>
+        <a href="/products">Products</a>
         <a href="/softwares">Softwares</a>
         <a href="/tools">Tools</a>
-        <a href="/music">Music</a>
         <a href="/contact">Contact</a>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 }
 
